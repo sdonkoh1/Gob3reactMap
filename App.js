4d7db57996d4a649a2c2a7e8053353b96442b269
sdonkoh1 <sdonkoh1@users.noreply.github.com>
2020-10-07 14:03:18 +0000

    one more time

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>again and again</Text>
+      <Text>one more time</Text>
       <StatusBar style="auto" />
     </View>
   );
