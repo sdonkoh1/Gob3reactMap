42598a0a7298a2488519ac02ade9b45f3243f43d
sdonkoh1 <sdonkoh1@users.noreply.github.com>
2020-10-07 14:01:15 +0000

    Add all this commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>All this commit</Text>
       <StatusBar style="auto" />
     </View>
   );
