e9ce3b7b64c64893fd8109fae8e3d2eb1175df12
sdonkoh1 <sdonkoh1@users.noreply.github.com>
2020-10-07 14:02:31 +0000

    another changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>All this commit</Text>
+      <Text>again and again</Text>
       <StatusBar style="auto" />
     </View>
   );
